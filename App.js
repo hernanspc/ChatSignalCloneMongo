c26b3e08505f883382b873b4dfa6d3351616cbbd
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-07-25 18:41:13 -0500

    Register

--- App.js
+++ App.js
@@ -2,10 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import LoginScreen from './src/screens/LoginScreen';
+import RegisterScreen from './src/screens/RegisterScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useFonts } from 'expo-font';
 
 const Stack = createNativeStackNavigator();
+
+const globalScreenOptions = {
+  headerStyle: { backgroundColor: '#2C6BED' },
+  headerTitleStyle: { color: 'white' },
+  headerTintColor: 'white'
+}
 
 export default function App() {
   const [loaded] = useFonts({
@@ -22,12 +29,18 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={globalScreenOptions}
+      >
         <Stack.Screen options={{
           title: 'Lets Sign Up',
         }}
           name="Login"
           component={LoginScreen}
+        />
+        <Stack.Screen
+          name="Register"
+          component={RegisterScreen}
         />
       </Stack.Navigator>
     </NavigationContainer>
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,10 +1,14 @@
+import React, { useEffect, useState } from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import React, { useEffect } from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, KeyboardAvoidingView } from 'react-native'
 import { useNavigation } from '@react-navigation/native';
+import { Button, Input, Image } from 'react-native-elements'
 
 const LoginScreen = () => {
     const navigation = useNavigation();
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
     // useEffect(() => {
     //     navigation.setOptions({
@@ -39,18 +43,62 @@ const LoginScreen = () => {
     //     });
     // }, [navigation]);
 
+    const signIn = () => {
+        console.log('signIn called')
+    }
+
     return (
-        <View style={styles.container}>
-            <StatusBar style="auto" />
-            {/* <Text>SIGNAL</Text> */}
-            <Text style={{
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
+            <StatusBar style="light" />
+            <Image
+                source={{
+                    uri: "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png"
+                    // uri: "https://www.pelucaspacos.com/imagenes/Logo_Signal.png"
+                    // uri: "https://play-lh.googleusercontent.com/jCln_XT8Ruzp7loH1S6yM-ZzzpLP1kZ3CCdXVEo0tP2w5HNtWQds6lo6aLxLIjiW_X8"
+                }}
+                style={{ width: 200, height: 200 }}
+            />
+            <View style={styles.inputContainer}>
+                <Input
+                    placeholder='Email'
+                    autoFocus
+                    type="email"
+                    value={email}
+                    onChangeText={(text) => setEmail(text)}
+                />
+                <Input
+                    placeholder='Password'
+                    secureTextEntry
+                    autoFocus
+                    type="password"
+                    value={password}
+                    onChangeText={(text) => setPassword(text)}
+                />
+
+                <Button
+                    containerStyle={styles.button}
+                    onPress={signIn}
+                    title="Login"
+                />
+                <Button
+                    containerStyle={styles.button}
+                    onPress={() => {
+                        navigation.navigate('Register');
+                    }}
+                    type="outline"
+                    title="Register"
+                />
+
+            </View>
+            <View style={{ height: 100 }}></View>
+            {/* <Text style={{
                 fontFamily: 'SpaceMonoRegular',
                 // fontWeight: 'bold',
                 fontSize: 30
             }}>Montserrat</Text>
-            <Text style={{ fontSize: 30 }}>Texto Normal</Text>
+            <Text style={{ fontSize: 30 }}>Texto Normal</Text> */}
 
-        </View>
+        </KeyboardAvoidingView>
     )
 }
 
@@ -59,8 +107,17 @@ export default LoginScreen
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+        padding: 10,
+        backgroundColor: 'white'
     },
+    inputContainer: {
+        alignItems: 'center',
+        width: 300
+    },
+    button: {
+        width: 200,
+        marginTop: 10,
+    }
 })
