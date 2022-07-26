6075d7b4c433b5d26eb506dbe0bfcff0261be68c
Hernan Pormachi Quichca <hernan.pormachi.quichca@everis.nttdata.com>
2022-07-26 15:11:22 -0500

    profile screen

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,9 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useContext } from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { AuthContext } from '../auth/AuthContext';
+import { Avatar, Button } from 'react-native-elements'
 
 const ProfileScreen = () => {
+    const { auth } = useContext(AuthContext);
+    const { uid, photoUrl } = auth;
     return (
         <View>
+            <Avatar
+                size="xlarge"
+                rounded
+                source={photoUrl ?
+                    {
+                        uri: `data:image/jpeg;base64,${photoUrl}`
+                    }
+                    :
+                    {
+                        uri: "https://www.scottsdirectories.com/wp-content/uploads/2017/10/default.jpg"
+                    }
+                } >
+
+                <Avatar.Accessory onPress={() => console.log('Press')} size={40} />
+
+            </Avatar>
             <Text>ProfileScreen</Text>
         </View>
     )
